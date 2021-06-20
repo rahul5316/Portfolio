2943101361a775dc656968586f9020289e84bb7b
Rahul <rahulkumarsinha5316@gmail.com>
2021-06-21 02:37:56 +0530

    Sixth Commit

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -3,7 +3,7 @@ import "./education.scss"
 export default function Education() {
   return (
     <div className="education" id ="education">
-      <h1>Hi I am Rahul</h1>
+      <h1>Hi, I am Rahul</h1>
     </div>
   )
 }
